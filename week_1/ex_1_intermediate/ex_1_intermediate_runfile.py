3de79a6c0db62801f0004bd98e6df010eea2a9cd
Paul Roque <p.roque11@hotmail.com>
2025-11-04 18:53:03 +0100

    i respond to the question

--- week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
+++ week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
@@ -1,1 +1,17 @@
-#YOUR CODE FOR EX_1 INTERMEDIATE HERE
+species = {
+    "bacteria" : 20,
+    "archaea" : 15,
+    "fungi" : 10
+}
+while True:
+    new_species =  input("entrer la nouvelle espece") #we want to write the potentially new species
+    if new_species=="stop": # with this if we can stop the while loop when ever we want just by typing "stop"
+        print("vous n'avez plus de nouvelle espéce") #display a message "you don't have new species anymore"
+        break
+    else:
+        if new_species in list(species): #if it's a new species, add to the list and give +1
+                species[new_species] += 1
+                print(species)
+        else:
+            species[new_species] = 1 #if it's not a new species, add +1 to the species already there
+            print(species)
